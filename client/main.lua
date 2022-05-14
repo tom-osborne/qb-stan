5b2278f8fcb5e3f24f912988076c7b38d33ebb0e
tom-osborne <tom.j.osborne@gmail.com>
2022-05-14 12:50:13 +0100

    setup as template

--- client/main.lua
+++ client/main.lua
@@ -1,49 +1,16 @@
 local QBCore = exports['qb-core']:GetCoreObject()
-local pedSpawned = false
-local ShopPed = {}
 
 RegisterNetEvent("QBCore:Client:OnPlayerLoaded", function()
     -- Initialise items here
 end)
 
-local function createPeds()
-    if pedSpawned then return end
-    for k, v in pairs(Config.Locations) do
-        if not ShopPed[k] then ShopPed[k] = {} end
-        local current = v["ped"]
-        current = type(current) == 'string' and GetHashKey(current) or current
-        RequestModel(current)
-
-        while not HasModelLoaded(current) do
-            Wait(0)
-        end
-        ShopPed[k] = CreatePed(0, current, v["coords"].x, v["coords"].y, v["coords"].z-1, v["coords"].w, false, false)
-        TaskStartScenarioInPlace(ShopPed[k], v["scenario"], true)
-        FreezeEntityPosition(ShopPed[k], true)
-        SetEntityInvincible(ShopPed[k], true)
-        SetBlockingOfNonTemporaryEvents(ShopPed[k], true)
-
-        if Config.UseTarget then
-            exports['qb-target']:AddTargetEntity(ShopPed[k], {
-                options = {
-                    {
-                        label = v["targetLabel"],
-                        icon = v["targetIcon"],
-                        action = function()
-                            openShop(k, Config.Locations[k])
-                        end,
-                        debug = true
-                    }
-                },
-                distance = 2.0
-            })
-        end
-    end
-    pedSpawned = true
+-- Functions
+local function myFunction()
+    -- Do stuff
+    print("Hello!")
 end
 
 -- Threads
-
 CreateThread(function()
     -- Tickrate of thread
     Wait(1000)
@@ -51,6 +18,6 @@ CreateThread(function()
     while true do
         -- Do stuff
         Wait(10000)
-        createPeds()
+        myFunction()
     end
 end)
